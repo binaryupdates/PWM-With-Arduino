814863897c3020026f583da528426d07651bf68f
BINARYUPDATES.COM <binaryupdates@gmail.com>
2018-01-16 17:46:50 +0530

    Add files via upload

---
+++ PWM With Arudino Brightness Control.ino
@@ -0,0 +1,23 @@
+int pwmPin = 6;
+
+void setup() {
+ pinMode(pwmPin, OUTPUT);
+}
+
+void loop() {
+
+  for(int value = 0; value<=255; value++){
+    analogWrite(pwmPin, value);
+    delay(30);
+    }
+
+  delay(50);
+
+  for(int value = 255; value>=0; value--){
+     analogWrite(pwmPin, value);
+    delay(30);
+    }
+    
+  delay(50);
+
+}
